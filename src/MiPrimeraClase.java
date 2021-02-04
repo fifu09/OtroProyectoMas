f2cf4d2d7e2b0301258b8e1bf9cdb5a327fbc4b4
docencia <docencia@U033311.vp.ehu.es>
2021-02-04 15:55:46 +0100

    otra version

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -3,7 +3,12 @@ public class MiPrimeraClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("este es un mensaje");
+		
+	}
+	public static void otroMetodo() {
+		System.out.print("este es un metodo");
+		System.out.print("este es un mensaje de la nueva version");
 	}
 
 }
